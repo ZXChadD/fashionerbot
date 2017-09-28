e8c118bcfae7d07818a71a31ffcbaa07737c983a
ZXChadd <zhexianchadd@gmail.com>
2017-09-29 00:13:12 +0800

    Add heroku database

--- dbhelper.py
+++ dbhelper.py
@@ -1,11 +1,24 @@
 import psycopg2
+import urllib.parse as urlparse
+import os
+
 
 class DBHelper:
     def __init__(self):
-        conn_string = "host='localhost' dbname='fashioner' user='postgres' password='1e9h2z!x'"
-        # self.dbname = dbname
-        # self.conn = sqlite3.connect(dbname)
-        self.conn = psycopg2.connect(conn_string)
+        url = urlparse.urlparse(os.environ['DATABASE_URL'])
+        dbname = url.path[1:]
+        user = url.username
+        password = url.password
+        host = url.hostname
+        port = url.port
+
+        conn = psycopg2.connect(
+                    dbname=dbname,
+                    user=user,
+                    password=password,
+                    host=host,
+                    port=port
+                    )
 
 
     def setup(self):

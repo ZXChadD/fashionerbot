78831e55b794e269016dcbba3fb8ba40700452d7
ZXChadd <zhexianchadd@gmail.com>
2017-09-29 00:00:27 +0800

    --amend

--- dbhelper.py
+++ dbhelper.py
@@ -1,36 +1,43 @@
-import sqlite3
+import psycopg2
 
 class DBHelper:
-    def __init__(self, dbname="fashioner.sqlite"):
-        self.dbname = dbname
-        self.conn = sqlite3.connect(dbname)
+    def __init__(self):
+        conn_string = "host='localhost' dbname='fashioner' user='postgres' password='1e9h2z!x'"
+        # self.dbname = dbname
+        # self.conn = sqlite3.connect(dbname)
+        self.conn = psycopg2.connect(conn_string)
+
 
     def setup(self):
-        stmt = "CREATE TABLE IF NOT EXISTS items (clothes_type text, result text, chat_id text, file_id text, status text)"
-        self.conn.execute(stmt)
+        cursor = self.conn.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS items (clothes_type text, result text, chat_id text, file_id text, status text)")
         self.conn.commit()
 
     def add_item(self, item_clothes_type, item_result, item_chat_id, item_file_id):
-        stmt = "INSERT INTO items (clothes_type, result, chat_id, file_id, status) VALUES (?, ?, ?, ?, ?)"
+        cursor = self.conn.cursor()
+        stmt = "INSERT INTO items (clothes_type, result, chat_id, file_id, status) VALUES (%s, %s, %s, %s, %s)"
         args = (item_clothes_type, item_result, item_chat_id, item_file_id, "nworn")
-        self.conn.execute(stmt, args)
+        cursor.execute(stmt, args)
         self.conn.commit()
 
     def reset_items(self, item_chat_id):
-        stmt = "UPDATE items SET status = 'nworn' WHERE chat_id = (?) "
+        cursor = self.conn.cursor()
+        stmt = "UPDATE items SET status = 'nworn' WHERE chat_id = '%s' "
         args = (item_chat_id, )
-        self.conn.execute(stmt, args)
+        cursor.execute(stmt, args)
         self.conn.commit()
 
     def get_items(self, item_chat_id):
-        stmt = "SELECT clothes_type, file_id FROM items WHERE chat_id = (?) and status = (?)"
+        cursor = self.conn.cursor()
+        stmt = "SELECT clothes_type, file_id FROM items WHERE chat_id = ('%s') and status = (%s)"
         args = (item_chat_id, 'nworn')
-        cursor = self.conn.execute(stmt,args)
+        cursor.execute(stmt, args)
         results = cursor.fetchall()
         return results
 
     def accept_items(self, item_chat_id, item_file_id):
-        stmt = "UPDATE items SET status = 'worn' WHERE chat_id = (?) and file_id = (?) "
+        cursor = self.conn.cursor()
+        stmt = "UPDATE items SET status = 'worn' WHERE chat_id = ('%s') and file_id = (%s) "
         args = (item_chat_id, item_file_id, )
-        self.conn.execute(stmt, args)
+        cursor.execute(stmt, args)
         self.conn.commit()

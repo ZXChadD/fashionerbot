207bdcf561cd1215db868658c5b733f15ac5882d
ZXChadd <zhexianchadd@gmail.com>
2017-09-28 21:08:55 +0800

    Add token for telegram

--- simple_bot_using_telepot.py
+++ simple_bot_using_telepot.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import time
 import random
@@ -124,7 +125,7 @@ def handle(msg):
 
     run()
 
-bot = telepot.Bot(token=TOKEN)
+bot = telepot.Bot(token='TOKEN')
 MessageLoop(bot, handle).run_forever()
 
 
